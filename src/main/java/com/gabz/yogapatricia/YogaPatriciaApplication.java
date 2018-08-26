2ac15476705c677166eb2d19e6e2ff45b1a99578
Gabz98 <gabinritz@gmail.com>
2018-08-26 11:28:29 +0200

    fist commit

---
+++ src/main/java/com/gabz/yogapatricia/YogaPatriciaApplication.java
@@ -0,0 +1,12 @@
+package com.gabz.yogapatricia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogaPatriciaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YogaPatriciaApplication.class, args);
+    }
+}
---
+++ src/main/java/com/gabz/yogapatricia/model/Student.java
@@ -0,0 +1,46 @@
+package com.gabz.yogapatricia.model;
+
+public class Student {
+
+    private int id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String phone;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
